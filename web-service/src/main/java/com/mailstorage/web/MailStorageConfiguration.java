6a28c24df6310cad3ce95bac7ca6517778c3caa1
Vladimir Polyakov <vap.polyakov@gmail.com>
2017-03-12 00:13:00 +0300

    narrow logging and fix PropertySourcesPlaceholderConfigurer bean

--- web-service/src/main/java/com/mailstorage/web/MailStorageConfiguration.java
+++ web-service/src/main/java/com/mailstorage/web/MailStorageConfiguration.java
@@ -37,7 +37,7 @@ public class MailStorageConfiguration {
     }
 
     @Bean
-    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
 }
